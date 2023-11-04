9c6f8800809602a12115d777f7eb44ef4249cce0
Tanjina sultana <tanjinasultana388@gmail.com>
2023-11-05 01:42:25 +0600

    extra section added

---
+++ src/components/routes/HomePage/CallToAction.jsx
@@ -0,0 +1,121 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+
+const CallToAction = () => {
+    return (
+        <div>
+            <section className="dark:bg-gray-800 dark:text-gray-100">
+	<div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
+		<div>
+			<h2 className="text-3xl font-bold tracki text-center sm:text-5xl dark:text-gray-50">Adventures Await – Answer the Call</h2>
+			<p className="max-w-3xl mx-auto mt-4 text-base text-center dark:text-gray-400">Get Ready to <br></br> Wander Off the Beaten Path Join Us!<br></br>Curate Your Own Adventure with Our Expert Guides..</p>
+		</div>
+		<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+			<div>
+				<h3 className="text-2xl font-bold tracki sm:text-3xl dark:text-gray-50">Be Our Guests</h3>
+				<p className="mt-3 text-base dark:text-gray-400">Grab Your Offer & Gifts Make A <br></br> Incridible Memory With Us In This Short Life.</p>
+				<div className="mt-12 space-y-12">
+					<div className="flex">
+						<div className="flex-shrink-0">
+							<div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+								</svg>
+							</div>
+						</div>
+						<div className="ml-4">
+							<h4 className="text-lg font-medium leadi dark:text-gray-50">Mystery Discounts</h4>
+							<p className="mt-2 dark:text-gray-400">Offer Our Guests a surprise discount where they Even don't know the exact amount until checkout..</p>
+						</div>
+					</div>
+					<div className="flex">
+						<div className="flex-shrink-0">
+							<div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+								</svg>
+							</div>
+						</div>
+						<div className="ml-4">
+							<h4 className="text-lg font-medium leadi dark:text-gray-50">Sunset Cruises</h4>
+							<p className="mt-2 dark:text-gray-400">Offer boat tours that take Our Guests on scenic sunset cruises, complete with dinner or drinks..</p>
+						</div>
+					</div>
+					<div className="flex">
+						<div className="flex-shrink-0">
+							<div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+								</svg>
+							</div>
+						</div>
+						<div className="ml-4">
+							<h4 className="text-lg font-medium leadi dark:text-gray-50">Professional Photography</h4>
+							<p className="mt-2 dark:text-gray-400">Provide professional photographers who capture customers' memories during the tour, delivering high-quality photographs.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div aria-hidden="true" className="mt-10 lg:mt-0">
+				<img src="https://source.unsplash.com/random/360x480" alt="" className="mx-auto rounded-lg shadow-lg dark:bg-gray-500" />
+			</div>
+		</div>
+		<div>
+			<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+				<div className="lg:col-start-2">
+					<h3 className="text-2xl font-bold tracki sm:text-3xl dark:text-gray-50">Choose Our Services.</h3>
+					<p className="mt-3 text-lg dark:text-gray-400">Make Your Journey Smother With Us..</p>
+					<div className="mt-12 space-y-12">
+						<div className="flex">
+							<div className="flex-shrink-0">
+								<div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+									</svg>
+								</div>
+							</div>
+							<div className="ml-4">
+								<h4 className="text-lg font-medium leadi dark:text-gray-50">Educational Expeditions.</h4>
+								<p className="mt-2 dark:text-gray-400"> Focus on educational tours, where guides provide insights into history, geology, flora, and fauna...</p>
+							</div>
+						</div>
+						<div className="flex">
+							<div className="flex-shrink-0">
+								<div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+									</svg>
+								</div>
+							</div>
+							<div className="ml-4">
+								<h4 className="text-lg font-medium leadi dark:text-gray-50">Emotional Customer Stories</h4>
+								<p className="mt-2 dark:text-gray-400">Share heartfelt and emotional stories from your customers about how your product or service impacted their lives..</p>
+							</div>
+						</div>
+						<div className="flex">
+							<div className="flex-shrink-0">
+								<div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+									</svg>
+								</div>
+							</div>
+							<div className="ml-4">
+								<h4 className="text-lg font-medium leadi dark:text-gray-50">Flashback Sales</h4>
+								<p className="mt-2 dark:text-gray-400">Occasionally offer products or services at prices similar to what they were years ago, creating a sense of nostalgia and excitement.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="mt-10 lg:mt-0 lg:col-start-1 lg:row-start-1">
+					<img src="https://source.unsplash.com/random/361x481" alt="" className="mx-auto rounded-lg shadow-lg dark:bg-gray-500" />
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+        </div>
+    );
+};
+
+export default CallToAction;
--- src/components/routes/HomePage/Home.jsx
+++ src/components/routes/HomePage/Home.jsx
@@ -1,10 +1,14 @@
 import Banner from "./Banner";
+import CallToAction from "./CallToAction";
+import Gallery from "./Gallery";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Gallery></Gallery>
+            <CallToAction></CallToAction>
         </div>
     );
 };
